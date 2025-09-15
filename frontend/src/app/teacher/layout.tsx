25429ed7385d722790393d6057a2cabd156051e7
ivannissimrch <ivannissimrch@gmail.com>
2025-09-15 11:47:14 -0400

    Add functionality to add new lessons

--- frontend/src/app/teacher/layout.tsx
+++ frontend/src/app/teacher/layout.tsx
@@ -1,13 +1,19 @@
+"use client";
 import Footer from "../components/Footer";
 import styles from "./layout.module.css";
 import Header from "../components/Header";
 import Link from "next/link";
+import { useState } from "react";
+import AddLesson from "../components/AddLesson";
 
 export default function LessonsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isAddLessonDialogOpen, setIsAddLessonDialogOpen] = useState(false);
+  const closeAddLessonDialog = () => setIsAddLessonDialogOpen(false);
+
   return (
     <>
       <Header />
@@ -25,14 +31,24 @@ export default function LessonsLayout({
         </aside>
         <main className={styles.mainContent}>{children}</main>
         <aside className={styles.rightSidebar}>
-          <button className={styles.sidebarButton}>Add lesson</button>
+          <button
+            className={styles.sidebarButton}
+            onClick={() => setIsAddLessonDialogOpen(true)}
+          >
+            Add lesson
+          </button>
           <Link href="/teacher/add-student" className={styles.sidebarButton}>
             Add student
           </Link>
           <button className={styles.sidebarButton}>Message student</button>
         </aside>
       </section>
+
       <Footer />
+      <AddLesson
+        isAddLessonDialogOpen={isAddLessonDialogOpen}
+        closeAddLessonDialog={closeAddLessonDialog}
+      />
     </>
   );
 }
