085850224b84565cf97424b5a7c16fd4d9816f3a
ivannissimrch <ivannissimrch@gmail.com>
2025-09-17 19:41:57 -0400

    Update lesson_id name on useSelectedLesson to fix deployment

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const initDatabase = async () => {
         assigned_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         completed_at TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        UNIQUE(student_id, lesson_id)
+        UNIQUE(student_id, lesson_id)lesson_id
       );
     `);
 
--- frontend/src/app/hooks/useSelectedLesson.ts
+++ frontend/src/app/hooks/useSelectedLesson.ts
@@ -5,9 +5,7 @@ export default function useSelectedLesson() {
   const lessonId = params.id;
   const { lessons } = useAppContext();
   if (lessons) {
-    const activeLesson = lessons.find(
-      (lesson) => lesson.lesson_id === lessonId
-    );
+    const activeLesson = lessons.find((lesson) => lesson.id === lessonId);
     return activeLesson;
   }
   return null;
