742ce4f8cc8303cd61b010cf613b05112b09406e
ivannissimrch <ivannissimrch@gmail.com>
2025-10-03 12:47:56 -0400

    Update login page layout, remove parrot iage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,9 @@
 import styles from "./page.module.css";
 import LoginForm from "./components/LoginForm";
-import Image from "next/image";
-import AnimatedMessage from "./components/AnimatedMessage";
 
 export default async function Login() {
   return (
     <main className={styles.main}>
-      <div className={styles["login-image"]}>
-        <AnimatedMessage />
-        <Image
-          src="/images/parrot.png"
-          alt="login image background"
-          width={250}
-          height={300}
-          sizes="(max-width: 300px) 100vw, 50vw"
-          priority
-        />
-      </div>
       <div className={styles["login-form"]}>
         <LoginForm />
       </div>
