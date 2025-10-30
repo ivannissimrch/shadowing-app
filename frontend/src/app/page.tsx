02a11225290beb5a5e829a91f1d7aaa18d7d70a4
ivannissimrch <ivannissimrch@gmail.com>
2025-10-30 07:48:58 -0400

    Ensure only one <main> on the login page (fixes accessibility error)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,9 @@ import LoginForm from "./components/auth/LoginForm";
 export default async function Login() {
   return (
     <main className={styles.main}>
-      <div className={styles["login-form"]}>
+      <section className={styles["login-form"]}>
         <LoginForm />
-      </div>
+      </section>
     </main>
   );
 }
