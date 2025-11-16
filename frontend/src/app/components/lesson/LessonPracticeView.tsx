073d21b228c8026cc480130fc8d3b136386b1e20
ivannissimrch <ivannissimrch@gmail.com>
2025-11-15 20:45:40 -0500

    Fix error boundary no reseting

--- frontend/src/app/components/lesson/LessonPracticeView.tsx
+++ frontend/src/app/components/lesson/LessonPracticeView.tsx
@@ -1,7 +1,5 @@
 "use client";
-import styles from "./LessonPracticeView.module.css";
 import RecorderPanel from "../media/RecorderPanel";
-import { ErrorBoundary } from "react-error-boundary";
 import { Lesson } from "../../Types";
 import { useSWRAxios } from "../../hooks/useSWRAxios";
 import { API_PATHS } from "../../constants/apiKeys";
@@ -12,13 +10,7 @@ export function LessonPracticeView({ id }: { id: string }) {
   const { data: selectedLesson } = useSWRAxios<Lesson>(API_PATHS.LESSON(id));
 
   return (
-    <ErrorBoundary
-      fallback={
-        <div className={styles.error}>
-          <h1>Error loading lesson.</h1>
-        </div>
-      }
-    >
+    <>
       {selectedLesson && (
         <>
           <VideoScriptToggle selectedLesson={selectedLesson} />
@@ -27,6 +19,6 @@ export function LessonPracticeView({ id }: { id: string }) {
           </RecorderPanelContextProvider>
         </>
       )}
-    </ErrorBoundary>
+    </>
   );
 }
--- frontend/src/app/lessons/[id]/page.tsx
+++ frontend/src/app/lessons/[id]/page.tsx
@@ -4,6 +4,8 @@ import { ErrorBoundary } from "react-error-boundary";
 import PracticePageSkeleton from "@/app/components/ui/PracticePageSkeleton";
 import { Suspense, use } from "react";
 import ErrorFallback from "@/app/components/ui/ErrorFallback";
+import { mutate } from "swr";
+import { API_PATHS } from "@/app/constants/apiKeys";
 
 export default function Practice({
   params,
@@ -12,7 +14,14 @@ export default function Practice({
 }) {
   const { id } = use(params);
   return (
-    <ErrorBoundary fallbackRender={(props) => <ErrorFallback {...props} />}>
+    <ErrorBoundary
+      fallbackRender={(props) => (
+        <ErrorFallback {...props} title="Error Loading Lesson" />
+      )}
+      onReset={() => {
+        mutate(API_PATHS.LESSON(id), undefined, { revalidate: true });
+      }}
+    >
       <Suspense fallback={<PracticePageSkeleton />}>
         <LessonPracticeView id={id} />
       </Suspense>
