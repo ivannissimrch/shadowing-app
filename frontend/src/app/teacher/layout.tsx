4a15b45ca13972183764ad3461284582fed07a37
ivannissimrch <ivannissimrch@gmail.com>
2025-11-30 15:36:43 -0500

    Refactor usePersistedState to use useSyncExternalStore

--- frontend/src/app/teacher/layout.tsx
+++ frontend/src/app/teacher/layout.tsx
@@ -1,12 +1,27 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Footer from "../components/layout/Footer";
 import TeacherHeader from "../components/layout/TeacherHeader";
+import { useAppContext } from "../AppContext";
 import styles from "./layout.module.css";
 
-export default function LessonsLayout({
+export default function TeacherLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { token } = useAppContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!token) {
+      router.push("/");
+    }
+  }, [token, router]);
+
+  if (!token) return null;
+
   return (
     <>
       <TeacherHeader />
